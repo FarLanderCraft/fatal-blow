7798d769f9e640ba122a8a831fc7c3241242f990
FarlanderCraft <javafarlander2002@gmail.com>
2021-08-13 19:16:02 -0400

    1.7.10 LESS GOOOOOOO!!!!

--- src/main/java/im/hunnybon/mobsplosion/mixin/ItemEntityMixin.java
+++ src/main/java/im/hunnybon/mobsplosion/mixin/ItemEntityMixin.java
@@ -2,12 +2,10 @@ package im.hunnybon.mobsplosion.mixin;
 
 import im.hunnybon.mobsplosion.Mobsplosion;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +18,7 @@ abstract class ItemEntityMixin extends Entity {
 
     @Shadow private int age;
 
-    @Shadow public abstract ItemStack getItemStack();
+    @Shadow public abstract ItemStack method_10736();
 
     public ItemEntityMixin(World world) {
         super(world);
@@ -36,8 +34,8 @@ abstract class ItemEntityMixin extends Entity {
     @Inject(method = "tick", at = @At("TAIL"))
     public void tick(CallbackInfo ci){
         if (!this.world.isClient && this.age >= 6000) {
-            this.world.createExplosion(this, this.x, this.y, this.z,
-                    this.getItemStack().getMaxCount() / 7.0f, Mobsplosion.config.destroyBlocks, Mobsplosion.config.createsFire);
+            this.world.method_330(this, this.x, this.y, this.z,
+                    this.method_10736().getMaxCount() / 7.0f, Mobsplosion.config.destroyBlocks, Mobsplosion.config.createsFire);
             this.remove();
         }
     }
--- src/main/java/im/hunnybon/mobsplosion/mixin/LivingEntityMixin.java
+++ src/main/java/im/hunnybon/mobsplosion/mixin/LivingEntityMixin.java
@@ -3,12 +3,10 @@ package im.hunnybon.mobsplosion.mixin;
 
 import im.hunnybon.mobsplosion.Mobsplosion;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,7 +25,7 @@ abstract class LivingEntityMixin extends Entity {
     @Inject(method = "onKilled", at = @At(value = "HEAD"))
     public void onDeath(DamageSource source, CallbackInfo ci){
         if (this.getMaxHealth() > 2.0f & this.world instanceof ServerWorld){
-            this.world.createExplosion(this, this.x, this.y, this.z,
+            this.world.method_330(this, this.x, this.y, this.z,
                     this.getMaxHealth() / 7.0f, Mobsplosion.config.destroyBlocks, Mobsplosion.config.createsFire);
         }
     }
